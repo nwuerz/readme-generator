0a8f54dfa4a0d59c15c9d522779bcb6cb13863e9
nick wuerz <nawuerz@gmail.com>
2020-02-26 10:13:28 -0600

    added .gitignore file

--- index.js
+++ index.js
@@ -30,6 +30,21 @@ inquirer.prompt([
         ]
     },
     {
-        type: 
-    }
+        type: "input",
+        name: "command",
+        message: "what kind of command should be run to install dependencies?",
+    },
+    {
+        type:"input",
+        name:"contributing",
+        message:"what does the user need to know about contributing to the repo?"
+    },
+    {
+        type:"input",
+        name:"tests",
+        message:"what command should be run in order to run tests?"
+    },
+    //if you have any quesions about the repo,open an issue or contact "username" directly @ "user email"
 ]);
+
+fs.appendFile("readme.md", )
